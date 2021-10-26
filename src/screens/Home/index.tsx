3b3595d4ce3915610f221b7c75eb26c30f0d04fb
Yukihiro Yamashita <yukihiroyamashita99@gmail.com>
2021-10-26 20:02:57 -0300

    Criação do card car

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -11,7 +11,29 @@ import {
   HeaderContent
 } from './styles';
 
+import Car from '../../components/Car';
+
 const Home: React.FC = () => {
+  const carDataOne = {
+    brand: 'AUDI',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'AO DIA',
+      price: 100,
+    },
+    thumbnail:'https://png.monster/wp-content/uploads/2020/11/2018-audi-rs5-4wd-coupe-angular-front-5039562b.png'
+  };
+
+  const carDataTwo = {
+    brand: 'PORSHE',
+    name: 'Panamera',
+    rent: {
+      period: 'AO DIA',
+      price: 230,
+    },
+    thumbnail: 'https://www.pngkit.com/png/full/237-2375888_porsche-panamera-s.png'
+  };
+
   return (
     <Container>
       <StatusBar
@@ -30,6 +52,14 @@ const Home: React.FC = () => {
           </TotalCars>
         </HeaderContent>
       </Header>
+
+      <Car
+        data={carDataOne}
+      />
+
+      <Car
+        data={carDataTwo}
+      />
     </Container>
   )
 }
