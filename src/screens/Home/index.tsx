0c9ef120413131773781c1101e8b7f5c35d817a7
Yukihiro Yamashita <yukihiroyamashita99@gmail.com>
2021-10-26 19:30:42 -0300

    Criação do Header da Home

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import Logo from '../../assets/logo.svg';
+
+import { 
+  Container,
+  Header,
+  TotalCars,
+  HeaderContent
+} from './styles';
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="transparent"
+      />
+      <Header>
+        <HeaderContent>
+          <Logo 
+            width={RFValue(108)}
+            height={RFValue(12)}
+          />
+          <TotalCars>
+            Total de 12 carros
+          </TotalCars>
+        </HeaderContent>
+      </Header>
+    </Container>
+  )
+}
+
+export default Home;
